a63c0e74f65cda60c1d97dc46df8259b623f6ea2
Marco Enrico Piras <kikkomep@crs4.it>
2019-05-14 15:38:18 +0200

    Use timestamp-based name as default

--- experiments/run.sh
+++ experiments/run.sh
@@ -3,11 +3,11 @@
 set +x
 
 # set experiment name
-experiment_name="${1:-polystore-experiment}"
+experiment_name="${1:-polystore-experiment-$(date '+%Y%m%d-%H%M%S')}"
 # update values with the experiment name
 sed -ie  's/id:.*/id: "'${experiment_name}'"/' values.yaml
 # set namespace
-namespace="default" # notice that, due to the hdfs config-map 
+namespace="polystore-experiments" # notice that, due to the hdfs config-map
                     # 'default' namespace, only default namespace is valid
 MyDir=$(cd `dirname $0` && pwd)
 helm repo update
@@ -17,4 +17,4 @@ helm install --debug \
     --namespace "${namespace}" \
     --version 0.1.0 \
     charts/
-    
+
