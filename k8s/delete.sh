1f6cbab6ef9aa4f68d39a7d6072a92c93d942d5d
Marco Enrico Piras <kikkomep@crs4.it>
2019-08-29 18:21:33 +0200

    Update install/delete scripts

--- k8s/delete.sh
+++ k8s/delete.sh
@@ -3,16 +3,21 @@
 # load config
 source ./config.sh
 
-# 
-cd resources
+# use resources as working-dir
+cd resources || exit 99
 
 # generate and deploy
 helm template chart \
-     --namespace ${KUBE_NAMESPACE} \
-     --name ${MPI_CLUSTER_NAME} \
-     -f ${VALUES_PATH} \
-     -f ssh-key.yaml | kubectl -n ${KUBE_NAMESPACE} delete -f -
+     --namespace "${KUBE_NAMESPACE}" \
+     --name "${MPI_CLUSTER_NAME}" \
+     -f "${VALUES_PATH}/cluster.yaml" \
+     -f "${VALUES_PATH}/wrf.yaml" \
+     -f ssh-key.yaml | kubectl -n "${KUBE_NAMESPACE}" delete -f -
+
+kubectl delete cm --namespace "${KUBE_NAMESPACE}" "${MPI_CLUSTER_NAME}-config-data"
+
+# delete storage class
+kubectl delete sc nfs-${MPI_CLUSTER_NAME}
 
 # delete the namespace
-# TODO: uncomment to completely delete all resources
 kubectl delete namespace ${KUBE_NAMESPACE}
--- k8s/install.sh
+++ k8s/install.sh
@@ -3,33 +3,49 @@
 # load config
 source ./config.sh
 
-# 
-cd resources
+# change wd
+cd resources || exit 99
 
 # generate ssh keys
 ./gen-ssh-key.sh
 
 # create the namespace
-kubectl create namespace ${KUBE_NAMESPACE}
+kubectl create namespace "${KUBE_NAMESPACE}"
 
-# copy hdfs configmap
-../copy-hdfs-config-cm.sh
+# install nfs-server provisioner
+helm install --debug --namespace "${KUBE_NAMESPACE}" \
+           -f ${VALUES_PATH}/nfs-values.yaml \
+           --set storageClass.name="nfs-${MPI_CLUSTER_NAME}" \
+           stable/nfs-server-provisioner
+
+# create the volumes
+kubectl apply -f pvc-data.yaml
 
 # create ClusterRoleBinding
 kubectl apply -f cluster-role-binding.yaml
 
-# create the volume claim
-kubectl apply -f kube-openmpi-run-data-claim.yaml
+# copy hdfs configmap
+../copy-hdfs-config-cm.sh
+
+# create
+kubectl create configmap "${MPI_CLUSTER_NAME}-config-data" \
+        --namespace "${KUBE_NAMESPACE}" \
+        --from-file=${VALUES_PATH}
 
 # generate and deploy
 helm template chart \
-     --namespace ${KUBE_NAMESPACE} \
-     --name ${MPI_CLUSTER_NAME} \
-     -f ${VALUES_PATH} \
-     -f ssh-key.yaml | kubectl -n ${KUBE_NAMESPACE} apply -f -
+  --namespace "${KUBE_NAMESPACE}" \
+  --name "${MPI_CLUSTER_NAME}" \
+  -f "${VALUES_PATH}/cluster.yaml" \
+  -f "${VALUES_PATH}/wrf.yaml" \
+  -f ssh-key.yaml | kubectl -n "${KUBE_NAMESPACE}" apply -f -
 
 # wait until $MPI_CLUSTER_NAME-master is ready
-until kubectl get -n ${KUBE_NAMESPACE} po ${MPI_CLUSTER_NAME}-master | grep Running ;
-do
-    date; sleep 1; echo "Waiting for kube-openmpi...";
+until kubectl get -n "${KUBE_NAMESPACE}" po "${MPI_CLUSTER_NAME}-master" | grep Running; do
+  date
+  sleep 1
+  echo "Waiting for kube-openmpi..."
 done
+
+# show WRF master logs
+kubectl logs ${KUBE_NAMESPACE}-master -f -c mpi-master
