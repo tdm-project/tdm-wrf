d998f6ec49268fb0313fc01c920245fe6834c6ca
Marco Enrico Piras <kikkomep@crs4.it>
2019-09-11 21:49:20 +0200

    Allow to specify the name of the configMap to be copied

--- k8s/copy-hdfs-config-cm.sh
+++ k8s/copy-hdfs-config-cm.sh
@@ -1,9 +1,20 @@
 #!/bin/bash
 
-source_namespace=${1:-"hdfs"}
-target_namespace=${2:-"openmpi"}
+configmap_name_pattern=${1:-"hdfs-config"}
+source_namespace=${2:-"hdfs"}
+target_namespace=${3:-"openmpi"}
 
-config_map=$(kubectl get cm -n ${source_namespace} | grep hdfs-config | awk '{print $1}')
+if [[ -z ${configmap_name_pattern} ]]; then
+   echo "ConfigMap name cannot be empty!"
+   exit 99
+fi
+
+config_map=$(kubectl get cm -n ${source_namespace} | grep ${configmap_name_pattern} | awk '{print $1}')
+
+if [[ -z ${config_map} ]]; then
+   echo "Unable to find the ConfigMap ${configmap_name_pattern-config} on namespace ${source_namespace}"
+   exit 99
+fi
 
 kubectl get cm ${config_map} --namespace=${source_namespace} --export -o yaml |\
    kubectl apply --namespace=${target_namespace} -f -
